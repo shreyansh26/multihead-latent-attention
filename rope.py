0901e2dfb13f6a2ab8fb75d423492d5781c4ea4e
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-10-29 20:18:15 -0700

    Fix var names

--- rope.py
+++ rope.py
@@ -24,13 +24,13 @@ def reshape_for_broadcast(freqs_cis, x):
 def apply_rotary_emb(x, freqs_cis):
     # print("Original x szhape: ", x.shape)
     x_bsnh_2 = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2)) # Group as complex nums pair-wise - B, S, Q, H/2
-    # print("x shape: ", x_bsqh_2.shape)
+    # print("x shape: ", x_bsnh_2.shape)
     freqs_cis_1s1h_2 = reshape_for_broadcast(freqs_cis, x_bsnh_2)
     # print("freqs cis shape: ", freqs_cis.shape)
-    # print("x * freqs cis shape: ", (x_bsnh_2 * freqs_cis_1s1h2).shape)
-    # print("x * freqs cis: ", x_bsnh_2 * freqs_cis_1s1h2)
-    # print("torch.view_as_real(x * freqs cis) shape: ", torch.view_as_real(x_bsnh_2 * freqs_cis_1s1h2).shape)
-    # print("torch.view_as_real(x * freqs cis): ", torch.view_as_real(x_bsnh_2 * freqs_cis_1s1h2))
+    # print("x * freqs cis shape: ", (x_bsnh_2 * freqs_cis_1s1h_2).shape)
+    # print("x * freqs cis: ", x_bsnh_2 * freqs_cis_1s1h_2)
+    # print("torch.view_as_real(x * freqs cis) shape: ", torch.view_as_real(x_bsnh_2 * freqs_cis_1s1h_2).shape)
+    # print("torch.view_as_real(x * freqs cis): ", torch.view_as_real(x_bsnh_2 * freqs_cis_1s1h_2))
     x_out_bsqh_2 = torch.view_as_real(x_bsnh_2 * freqs_cis_1s1h_2).flatten(3)
     # print("x_out shape: ", x_out_bsqh_2.shape)
     # print("x_out: ", x_out_bsqh_2)
