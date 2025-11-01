b6ec70b8a7a2cffa534a0af1edefb571869c145f
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-10-31 22:04:07 -0700

    Add modular attention impls and add checks for both causal and non-causal case

--- mha.py
+++ mha.py
@@ -2,9 +2,9 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from einops import einsum
 from model_config import ModelConfig
 from rope import precompute_freqs_cis, apply_rotary_emb
+from attention import naive_attention, sdpa_attention
 
 """
 Variable suufix nomenclature:
@@ -43,7 +43,7 @@ class MHA(nn.Module):
         self.v_proj = nn.Linear(self.d_model, self.num_kv_heads * self.head_dim, bias=False, dtype=dtype)
         self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.d_model,bias=False, dtype=dtype)
 
-    def forward(self, x_bsd, return_torch_ref=False):
+    def forward(self, x_bsd, is_causal=False, return_torch_ref=False):
         batch_size, seq_len, d_model = x_bsd.shape
         new_shape = (batch_size, seq_len, -1, self.head_dim) # -1 because num_heads or num_kv_heads
         q_bsqh = self.q_proj(x_bsd).view(new_shape)
@@ -57,28 +57,12 @@ class MHA(nn.Module):
         k_bklh = k_blkh.transpose(1, 2)
         v_bklh = v_blkh.transpose(1, 2)
         
-        # Repeat K/V across query head groups
-        k_bqlh = k_bklh.repeat_interleave(self.num_kv_groups, dim=1)
-        v_bqlh = v_bklh.repeat_interleave(self.num_kv_groups, dim=1)
-
-        attn_bqsl = einsum(q_bqsh, k_bqlh, "b q s h, b q l h -> b q s l")
-        attn_bqsl = attn_bqsl / math.sqrt(self.head_dim)
-        attn_bqsl = F.softmax(attn_bqsl, dim=-1)
-        out_bqsh = einsum(attn_bqsl, v_bqlh, "b q s l, b q l h -> b q s h")
-        out_bsqh = out_bqsh.transpose(1, 2).contiguous()
-        out_bsd = out_bsqh.view(batch_size, seq_len, self.d_model)
+        out_bsd = naive_attention(q_bqsh, k_bklh, v_bklh, is_causal=is_causal)
         out_bsd = self.o_proj(out_bsd)
 
         # Torch implementation
         if return_torch_ref:
-            out_bsd_torch = torch.nn.functional.scaled_dot_product_attention(
-                q_bqsh,
-                k_bklh,
-                v_bklh,
-                enable_gqa=self.num_kv_groups > 1,
-                is_causal=False,
-            )
-            out_bsd_torch = out_bsd_torch.transpose(1, 2).contiguous().view(batch_size, seq_len, self.d_model)
+            out_bsd_torch = sdpa_attention(q_bqsh, k_bklh, v_bklh, is_causal=is_causal)
             out_bsd_torch = self.o_proj(out_bsd_torch)
             return out_bsd, out_bsd_torch
 
@@ -114,10 +98,14 @@ if __name__ == "__main__":
     # MHA
     model = MHA(model_config_mha, dtype=dtype).to(device)
     x_bsd = torch.randn(batch_size, seq_len, model_config_mha.d_model, dtype=dtype).to(device)
-    out_bsd, out_bsd_torch = model(x_bsd, return_torch_ref=True)
-
+    
+    out_bsd, out_bsd_torch = model(x_bsd, is_causal=False, return_torch_ref=True)
     torch.testing.assert_close(out_bsd, out_bsd_torch, atol=1e-4, rtol=1e-4)
-    print("MHA: All check passed.")
+    print("MHA: All check passed for non-causal case.")
+    
+    out_bsd, out_bsd_torch = model(x_bsd, is_causal=True, return_torch_ref=True)
+    torch.testing.assert_close(out_bsd, out_bsd_torch, atol=1e-4, rtol=1e-4)
+    print("MHA: All check passed for causal case.")
 
     # del model, x_bsd, out_bsd, out_bsd_torch
     # torch.cuda.empty_cache()
@@ -125,10 +113,14 @@ if __name__ == "__main__":
     # GQA
     model = MHA(model_config_gqa, dtype=dtype).to(device)
     x_bsd = torch.randn(batch_size, seq_len, model_config_gqa.d_model, dtype=dtype).to(device)
-    out_bsd, out_bsd_torch = model(x_bsd, return_torch_ref=True)
-
+   
+    out_bsd, out_bsd_torch = model(x_bsd, is_causal=False, return_torch_ref=True)
     torch.testing.assert_close(out_bsd, out_bsd_torch, atol=1e-4, rtol=1e-4)
-    print("GQA: All check passed.")
+    print("GQA: All check passed for non-causal case.")
+
+    out_bsd, out_bsd_torch = model(x_bsd, is_causal=True, return_torch_ref=True)
+    torch.testing.assert_close(out_bsd, out_bsd_torch, atol=1e-4, rtol=1e-4)
+    print("GQA: All check passed for causal case.")
 
     # del model, x_bsd, out_bsd, out_bsd_torch
     # torch.cuda.empty_cache()
@@ -136,10 +128,14 @@ if __name__ == "__main__":
     # MQA
     model = MHA(model_config_mqa, dtype=dtype).to(device)
     x_bsd = torch.randn(batch_size, seq_len, model_config_mqa.d_model, dtype=dtype).to(device)
-    out_bsd, out_bsd_torch = model(x_bsd, return_torch_ref=True)
-
+    
+    out_bsd, out_bsd_torch = model(x_bsd, is_causal=False, return_torch_ref=True)
     torch.testing.assert_close(out_bsd, out_bsd_torch, atol=1e-4, rtol=1e-4)
-    print("MQA: All check passed.")
+    print("MQA: All check passed for non-causal case.")
+
+    out_bsd, out_bsd_torch = model(x_bsd, is_causal=True, return_torch_ref=True)
+    torch.testing.assert_close(out_bsd, out_bsd_torch, atol=1e-4, rtol=1e-4)
+    print("MQA: All check passed for causal case.")
 
     # del model, x_bsd, out_bsd, out_bsd_torch
     # torch.cuda.empty_cache()
