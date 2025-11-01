437c066c2d2c09f87377c840482b595e0d3c2ebf
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-11-01 06:21:57 -0700

    Add MLA model config

--- model_config.py
+++ model_config.py
@@ -6,3 +6,15 @@ class ModelConfig:
     num_heads: int
     num_kv_heads: int
     max_seq_len: int
+
+@dataclass
+class ModelConfigMLA:
+    dim: int
+    q_lora_rank: int
+    kv_lora_rank: int
+    qk_rope_head_dim: int
+    qk_nope_head_dim: int
+    v_head_dim: int
+    num_key_value_heads: int
+    num_attention_heads: int
+    max_seq_len: int
