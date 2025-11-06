ea9bf06ace1b708f6a074f6bc6b96d746340ec08
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-11-06 04:19:30 -0800

    Add cache implementation for MLA

--- cache.py
+++ cache.py
@@ -16,3 +16,34 @@ class KVCacheMHA():
         self.v_cache[:, :, self.pos: self.pos + len_seq] = v
         self.pos += len_seq
         return self.k_cache[:, :, :self.pos], self.v_cache[:, :, :self.pos]
+
+class _CompressedKVCache():
+    def __init__(self, batch_size, max_tokens, kv_lora_rank, dtype=torch.bfloat16, device="cuda"):
+        self.cache = torch.empty((batch_size, max_tokens, kv_lora_rank), dtype=dtype, device=device)
+        self.pos = 0
+        
+    def update(self, compressed_kv):
+        len_seq = compressed_kv.shape[1]
+        self.cache[:, self.pos: self.pos + len_seq] = compressed_kv
+        self.pos += len_seq
+        return self.cache[:, :self.pos]
+
+class _KRopeCache():
+    def __init__(self, batch_size, max_tokens, qk_rope_head_dim, dtype=torch.bfloat16, device="cuda"):
+        self.cache = torch.empty((batch_size, 1, max_tokens, qk_rope_head_dim), dtype=dtype, device=device)
+        self.pos = 0
+        
+    def update(self, k_rope):
+        len_seq = k_rope.shape[2]
+        self.cache[:, :, self.pos: self.pos + len_seq] = k_rope
+        self.pos += len_seq
+        return self.cache[:, :, :self.pos]
+
+class CacheMLA():
+    def __init__(self, batch_size, max_tokens, model_config, dtype=torch.bfloat16, device="cuda"):
+        self.compressed_kv = _CompressedKVCache(batch_size, max_tokens, model_config.kv_lora_rank, dtype=dtype, device=device)
+        self.k_rope = _KRopeCache(batch_size, max_tokens, model_config.qk_rope_head_dim, dtype=dtype, device=device)
+        
+    @property
+    def pos(self):
+        return self.compressed_kv.pos
